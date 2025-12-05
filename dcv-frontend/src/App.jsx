98c385418cf878d2902f73e7237f52f2a8b7243c
onlykshv <therealkeshav05@gmail.com>
2025-12-05 23:00:43 +0530

    Initial project setup with Vite, React, Tailwind, and base structure

---
+++ dcv-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import LandingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0A0A0A] text-white">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
